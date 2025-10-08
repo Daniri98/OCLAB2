48f3efa4a3bbcdfbd0976c0828695d7c0409c00a
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-10-08 20:30:11 +0000

    ¨Practica¨

--- P5/p5a.asm
+++ P5/p5a.asm
@@ -5,8 +5,8 @@ section	.text
 	global _start       ;referencia para inicio de programa
 	
 _start:                   		
-    mov msg,'Z'
 	mov edx, msg        ; edx = dirección de la cadena msg	
+	 mov edx,'Z'
     call puts 			; imprime cadena msg terminada en valor nulo (0)
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
