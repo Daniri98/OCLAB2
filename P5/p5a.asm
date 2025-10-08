d7b0d737fe055e6d045b711f839a3be9bff8daf8
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-10-08 20:46:31 +0000

    ¨Practica¨

--- P5/p5a.asm
+++ P5/p5a.asm
@@ -6,7 +6,7 @@ section	.text
 	
 _start:                   
 			
-	mov al,0x5A
+	mov al,'Z'
 	mov	[msg],al
 	mov edx, msg ;  	; edx = dirección de la cadena msg
 	call puts			; imprime cadena msg terminada en valor nulo (0)
