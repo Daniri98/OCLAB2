134e32feb5e04ca11445c79b1b06602dba5fb50b
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-10-17 05:01:11 +0000

    Practica

--- P6/P6.asm
+++ P6/P6.asm
@@ -11,13 +11,15 @@ _start:                     ;tell linker entry point
     call pHex_dw
     mov al,10	; cambio de linea
 	call putchar
-    mov ax,sp
-    call pHex_w
+
+    mov ax,sp           ;mover a donde esta apuntando el stack pointer a AX
+    call pHex_w         ;Desplegar el valor inicial de sp
     mov al,10	; cambio de linea
 	call putchar
-    push  bx
-    mov ax,sp
-    call pHex_w
+
+    push bx              ; insertar los datos de 16 menos significativos de EBX
+    mov ax,sp            ; mover a donde esta apuntando SP a AX
+    call pHex_w          ;Desplegar en donde estaba apuntando
     mov al,10	; cambio de linea
 	call putchar
     
@@ -27,22 +29,30 @@ _start:                     ;tell linker entry point
     call pHex_w         ;Desplegar el resultado
     mov al,10	; cambio de linea
 	call putchar
-    inc word [N]       ;Incrementar en 1 el valor encontrado en el lugar de memoria de N
-    mov ax,[N]         ;Mover el valor incrementado a ax
-    call pHex_w        ;Despliegue del valor aumentado
+
+    inc word [N]         ;Incrementar en 1 el valor encontrado en el lugar de memoria de N
+    mov ax,[N]           ;Mover el valor incrementado a ax
+    call pHex_w          ;Despliegue del valor aumentado
     mov al,10	; cambio de linea
 	call putchar
+
     mov ax,bx           ;mover el valor del registro bx a ax para poder dividirlo
     mov cx, 0xFF        ;darle el valor de 0xFF al divisor
     div cx              ; dividir ax con cx
     call pHex_w         ; despliegue del cociente de la division
     mov al,10	; cambio de linea
 	call putchar
-    mov ax,dx       ;mover el valor del residuo a ax 
-    call pHex_w     ;despliegue del valor del residuo
+    mov ax,dx           ;mover el valor del residuo a ax 
+    call pHex_w         ;despliegue del valor del residuo
     mov al,10	; cambio de linea
 	call putchar
-
+    
+    mov ax,[N]
+    add ax,dx
+    call pHex_w  
+    mov [N],ax
+    mov al,10	; cambio de linea
+	call putchar
 
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
