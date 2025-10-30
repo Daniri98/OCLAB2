e4aeca3a1a66ee4423853f1aa38e223c60dff8b8
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-10-30 18:44:24 +0000

    11_45

--- P8/P8.asm
+++ P8/P8.asm
@@ -46,15 +46,17 @@ call puts
 mov al,10
 call putchar
 
-mov cx,-6
-inaster inc cx
-aster   mov ax,cx
-mov edx,risco
-call puts
-inc cx
-loope aster
-mov cx,ax
-loope inaster
+mov cx,6
+inaster mov bx,cx
+aster  mov al,[risco] 
+call putchar
+sub cx,1
+loop aster
+mov al,10
+call putchar
+mov cx,bx
+sub cx,1
+loop inaster
 
 mov eax, 1 ;system call number (sys_exit) -- fin del programa
 int 0x80        ;call kernel
@@ -64,4 +66,4 @@ msg db 'Menor_que_m',0xa,0
 fdr db 'Fuera_de_rango',0xa,0
 N db 'Es_un_numero',0xa,0
 L db 'Es_una_letra',0xa,0
-risco db '*',0xa,0
+risco db '*'
