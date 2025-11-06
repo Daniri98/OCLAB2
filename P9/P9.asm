c74f24aa730a8a5d512f8fdd0ae8a18432607b94
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-11-06 19:35:36 +0000

    commit de las 11:35

--- P9/P9.asm
+++ P9/P9.asm
@@ -1,14 +1,54 @@
-%include "../LIB/libpc_iox.a"
-global _start
-section .data
-vector db 12345
-section .dss
+%include "../LIB/pc_iox.inc"
+N equ 10
 
+global _start
+
+section .data
+tamano db 'Defina el tamano de su vector',0xa,0
+inval db 'Invalido favor introducir otro caracter',0xa,0
+section .bss
+xdd resb N
 
 section .text
+_start:
+mov edx,tamano
+call puts
+call CambioLinea
+mov ecx,N
+mov ebx,vector
+call introduvector
+call Desplvector
 
-
-
-mov eax, 1 
+mov eax,1
 int 0x80  
 
+Desplvector:
+    mov edx,ecx
+    continuo mov eax,[ebx+ecx-1]
+    call pHex_b
+    call CambioLinea
+    loop continuo
+    mov ecx,edx
+    ret
+
+CambioLinea:
+    push eax
+    mov al,10
+    call putchar
+    pop eax
+ret
+
+introduvector:
+mov edx,ecx
+captura  call getch
+    cmp '0'
+    JB
+    cmp'9'
+    JA invalido
+    mov byte
+    push edx
+    invalido mov edx,inval
+    call puts
+    pop edx
+    loop captura
+ret
