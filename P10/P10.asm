e6b6f263e505a447bf108d6477f8e90790ef1bf6
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-11-13 19:50:38 +0000

    11:50

---
+++ P10/P10.asm
@@ -0,0 +1,33 @@
+global suma
+global strlenn
+section .text
+
+suma:
+push ebp
+mov ebp,esp
+mov eax,[ebp+8]
+add eax,[ebp+12]
+pop ebp
+ret
+
+strlenn:
+push ebp
+mov ebp,esp
+push ecx
+push ebx
+push esi
+mov ecx,20
+mov eax,0
+contar:
+mov byte bl,[ebp+esi]
+cmp bl,0
+JE No
+inc eax
+inc esi
+loop contar
+No:
+pop esi
+pop ebx
+pop ecx
+pop ebp
+ret
---
+++ P10/P10c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+extern int suma(int,int);
+extern int strlenn(char*);
+int main(){
+    int a=5,b=12;
+    char cadena[20]={"Camarones"};
+    printf("El valor de la suma es %d\n",suma(a,b));
+    printf("El tamaño de la cadena es %d\n",strlenn(cadena));
+}
