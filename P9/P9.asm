4272385a295c7f5239c1473ee726b0279303e6e1
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-11-13 06:16:25 +0000

    Final P9

--- P9/P9.asm
+++ P9/P9.asm
@@ -6,8 +6,10 @@ global _start
 section .data
 tamano db 'Introdusca los valores del vector',0xa,0
 inval db 'Invalido favor introducir otro caracter',0xa,0
+fuera db 'El valor de N esta fuera del rango',0xa,0
 section .bss
 vector resb N
+vector2 resb N
 
 section .text
 _start:
@@ -15,21 +17,39 @@ mov edx,tamano
 call puts
 call CambioLinea
 mov ecx,N
+call Checkeo
+cmp al,1
+JE fin
 mov ebx,vector
 call introduvector
-call Desplvector
+mov edx,tamano
+call puts
+call CambioLinea
+mov ebx,vector2
+call introduvector
+mov ebx,vector
+mov edx,vector2
+call SumaVector
+call ProductoEscalarVector
 
+
+fin:
+xor ebx,ebx
 mov eax,1
 int 0x80  
 
 Desplvector:
-    mov edx,ecx
-    continuo mov eax,[ebx+ecx-1]
-    sub eax,030h
+    push ecx
+    push eax
+    push esi
+    continuo mov eax,[ebx+esi]
+    inc esi
     call pHex_b
     call CambioLinea
     loop continuo
-    mov ecx,edx
+    pop esi
+    pop eax
+    pop ecx
     ret
 
 CambioLinea:
@@ -40,21 +60,86 @@ CambioLinea:
 ret
 
 introduvector:
-mov edx,ecx
+push ecx
+push eax
+push edx
+push esi
 cc:
 captura  call getch
     cmp al,'0'
     JB invalido
     cmp al,'9'
     JA invalido
-    mov byte[ebx+ecx-1],al
+    sub eax,030h
+    mov byte[ebx+esi],al
+    inc esi
     jmp caminata
-    invalido    push edx
-    mov edx,inval
+    invalido   mov edx,inval
     call puts
-    pop edx
     jmp cc
     caminata:
    loop captura
-   mov ecx,edx
+   pop esi
+   pop edx
+   pop eax
+   pop ecx
+ret
+
+    ;modifica el valor de al en caso de ser fuera de rango
+Checkeo: 
+    push ecx
+    push edx
+    cmp ecx,10
+    JA xx
+    cmp ecx,1
+    JAE xxd
+    xx:
+    mov edx,fuera
+    call puts
+    call CambioLinea
+    mov al,1
+    xxd:
+    pop edx
+    pop ecx
+ret
+
+SumaVector:
+    push ecx
+    push ebx
+    push edx
+    push esi
+    push eax
+    sumando:
+        mov byte al,edx[esi]
+        add ebx[esi],al
+        inc esi
+    loop sumando
+    pop eax
+    pop esi
+    pop edx
+    pop ebx
+    pop ecx
+    call CambioLinea
+    call Desplvector
+ret
+
+ProductoEscalarVector:
+    push ecx
+    push ebx
+    push edx
+    push esi
+    push eax
+    multiplicar:
+    mov byte al,edx[esi]
+    mul byte ebx[esi]
+    mov byte ebx[esi],al
+    inc esi
+    loop multiplicar
+    pop eax
+    pop esi
+    pop edx
+    pop ebx
+    pop ecx
+    call CambioLinea
+    call Desplvector
 ret
