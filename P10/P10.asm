ad953940fc780a62b02946eb7a4abab2dedddc43
Daniri98 <daniel.rivera98@uabc.edu.mx>
2025-11-20 04:36:38 +0000

    Terminado

--- P10/P10.asm
+++ P10/P10.asm
@@ -1,5 +1,6 @@
 global suma
 global strlenn
+global getBit
 section .text
 
 suma:
@@ -26,6 +27,26 @@ inc edx
 jmp contar
 No:
 pop edx
+pop ebx
+pop ebp
+ret
+
+getBit:
+push ebp
+mov ebp,esp
+push ebx
+push ecx
+mov ecx,ebp[12]
+inc ecx
+mov ebx,ebp[8]
+ror ebx,cl
+JNC zzero
+mov eax,1
+jmp finnn
+zzero:
+mov eax,0
+finnn:
+pop ecx
 pop ebx
 pop ebp
 ret
--- P10/P10c.c
+++ P10/P10c.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 extern int suma(int,int);
 extern int strlenn(char*);
+extern int getBit(int,int);
 int main(){
-    int a=5,b=12;
+    int a=5,b=12,c=127,numbit;
     printf("El valor de la suma es %d\n",suma(a,b));
     printf("El tamaño de la cadena es %d\n",strlenn("Camarones"));
+    do{
+    printf("Introdusca el bit al que quiere acceder del (0 al 31) del numero %d\n",c);
+    scanf("%d",&numbit);
+    }while(numbit<0 || numbit>31);
+    printf("El bit en la posicion %d es %d del numero %d\n",numbit,getBit(c,numbit),c);
 }
